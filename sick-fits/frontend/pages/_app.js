5fb49ad40244a28bd6f7996b5afb7d98577c7c03
robcrockPI <robert@propellerindustries.com>
2020-09-24 06:16:07 -0300

    Created global styles and applied those styles to the Nav. Also made sure CSS was rendered server side where it makes sense.

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,16 +1,14 @@
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Page from '../components/Page';
 
 class MyApp extends App {
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps } = this.props;
 
     return (
-      <Container>
-        <Page>
-          <Component />
-        </Page>
-      </Container>
+      <Page>
+        <Component />
+      </Page>
     )
   }
 }
